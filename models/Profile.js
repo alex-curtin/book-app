9b208dd40c9b1a1a33c7a13f2eb7e5f97ab5b8a1
Alex <alexhcurtin@gmail.com>
2020-01-11 17:04:57 -0500

    add profile model and route

---
+++ models/Profile.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  location: {
+    type: String
+  },
+  bio: {
+    type: String
+  },
+  social: {
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
