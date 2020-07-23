b22886e871c18dc3cd827d0e8d07e014345cfda7
ahcurtin <acurtin@theride-edp.com>
2020-07-23 07:42:11 -0400

    prepare for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const logger = require('morgan');
+const path = require('path');
 
 const app = express();
 
@@ -20,7 +21,15 @@ app.use('/api/profiles', require('./routes/api/profiles'));
 app.use('/api/books', require('./routes/api/books'));
 app.use('/api/book-lists', require('./routes/api/bookLists'));
 
-app.get('/', (req, res) => res.send('API running'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
