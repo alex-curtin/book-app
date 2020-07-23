edfe88d17144eeb0de592dd0d4e1071a94ee553d
ahcurtin <acurtin@theride-edp.com>
2020-07-23 07:57:02 -0400

    fix deployment issues

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,16 +7,16 @@ module.exports = function (req, res, next) {
 
   // Check if no token
   if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' })
+    return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
   // Verify token
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    const decoded = jwt.verify(token, process.env.jwtSecret);
 
     req.user = decoded.user;
     next();
   } catch (err) {
     res.status(401).json({ msg: 'Token is not valid' });
   }
-}
+};
