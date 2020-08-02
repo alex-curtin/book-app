541be5cdd956c200595d75a4a7dd8b34a4144b43
ahcurtin <acurtin@theride-edp.com>
2020-08-02 11:59:25 -0400

    add loading component

--- client/src/components/layout/Loading.jsx
+++ client/src/components/layout/Loading.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import ReactLoading from 'react-loading';
+import styled from 'styled-components';
+import { setColor, setRem, setFlex } from './styles';
 
 const Loading = () => {
   return (
-    <div>
-      Loading...
-    </div>
-  )
-}
+    <LoadingWrapper>
+      <ReactLoading type='bars' color={setColor.primary} />
+    </LoadingWrapper>
+  );
+};
+
+const LoadingWrapper = styled.div`
+  margin: ${setRem()};
+  width: 100%;
+  ${setFlex()};
+`;
 
 export default Loading;
